27f411d3bdad302c2feb7208c8c39dc5ca5b2e48
SoulAryan <asuryavans21@gmail.com>
2024-10-14 19:12:53 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    //Declaration of a variable//
+    int i;
+    int j;
+    int k;
+
+    //Definition of a variable//
+    i=10;
+    j=11;
+
+    //Usage of a variable//
+
+    k=i+j;
+
+    printf("%d\n",k);
+
+
+    return 0;
+}
